b5fb46031092b89303f40cf0433b9fef110657d9
Pieter Spruijt <mollyheerlen@gmail.com>
2021-08-07 13:07:25 +0200

    fix

--- events/client/ready.js
+++
@@ -1,8 +0,0 @@
-const { AutoPoster } = require('topgg-autoposter')
-
-module.exports = async function (bot) {
-    const poster = AutoPoster(bot.config.topggToken, bot);
-    setInterval(() => {
-        const poster = AutoPoster(bot.config.topggToken, bot);
-    }, 1800000) // post every 30 minutes
-}
--- models/userdb.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-let userdb = new mongoose.Schema({
-  userid: { type: String },
-  developer: { type: Boolean, default: false },
-  banned: { type: Boolean, default: false },
-  color: { type: String, default: `#e91e63`},
-  snipe: { type: Boolean, default: true}
-});
-module.exports = mongoose.model("userdb", userdb);
